ec407858bf81482654275f5d7b0ac8ed69e3b9a9
Gaspar <e972136@gmail.com>
2025-06-04 10:08:36 -0600

    primero

---
+++ src/main/java/com/gaspar/unlimited_costos/UnlimitedCostosApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.unlimited_costos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlimitedCostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlimitedCostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspar/unlimited_costos/UnlimitedCostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspar.unlimited_costos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlimitedCostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
